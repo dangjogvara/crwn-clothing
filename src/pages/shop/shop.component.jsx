66a03dba4c7dd89b7c575496b4238b0539ef365e
Dan Poulsen <dangjogvara@outlook.com>
2022-01-21 21:26:40 +0000

    ShopPage and data

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collections: SHOP_DATA,
+    };
+  }
+
+  render() {
+    return <div>Shop Page</div>;
+  }
+}
