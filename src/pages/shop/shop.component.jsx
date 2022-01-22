78df77f07a8f269b85ee2739a3b2b3e56355531a
Dan Poulsen <dangjogvara@gmail.com>
2022-01-22 12:20:31 +0000

    map component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import SHOP_DATA from './shop.data';
 
+import CollectionPreview from '../../components/preview-collection/collection-preview.component';
+
 class ShopPage extends React.Component {
   constructor(props) {
     super(props);
@@ -12,7 +14,14 @@ class ShopPage extends React.Component {
   }
 
   render() {
-    return <div>Shop Page</div>;
+    const { collections } = this.state;
+    return (
+      <div className='shop-page'>
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
   }
 }
 
